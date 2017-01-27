befae153d6e5f4b0bcabcbecbb4528e4c189644d
keely Glenn <glenj392@newschool.edu>
2017-01-27 09:51:42 -0800

    make price functionality based only on size. does not add correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,47 @@
 // backend
 function Order() {
   this.feeds = 0;
-  this.toppingCount = 0;
-  this.priceSize = 0;
-  this.priceTopping = 0;
-  this.total = 5;
+  // this.toppingCount = 0;
+  this.priceSize = 1;
+  // this.priceTopping = 0;
+  this.total = 0;
 }
 
 
-var sizeType = parseInt($("input:radio[name=size]:checked").val());
-$("input:checkbox[name=top]:checked").each(function() {
-  var toppingType = parseInt($(this).val());
-  toppingCount++;
-});
+//
+// $("input:checkbox[name=top]:checked").each(function() {
+//   var toppingType = parseInt($(this).val());
+//   toppingCount++;
+// });
 
 var pizza = new Order();
-
+//
 Order.prototype.priceFeed = function() {
 // size
   if(this.feeds === 1) {
     this.priceSize += 1;
-  } else if (this.feeds === 2) {
+  } else if (this.feeds = 2) {
     this.priceSize += 2;
-  } else if (this.feeds === 3) {
-    this.priceSize =+ 3;
-  } else if (this.feeds === 4) {
+  } else if (this.feeds = 3) {
+    this.priceSize += 3;
+  } else if (this.feeds = 4) {
     this.priceSize += 4;
-	}
-};
-
-Order.prototype.priceTop = function () {
-// toppings
-  if (this.toppingCount <= 2) {
-    this.priceTopping += 1;
   } else {
-    this.priceTopping += 2;
+    this.priceSize = 0;
   }
 };
+//
+// Order.prototype.priceTop = function () {
+// // toppings
+//   if (this.toppingCount <= 2) {
+//     priceTopping += 1;
+//   } else {
+//     priceTopping += 2;
+//   }
+// };
 
 Order.prototype.priceAll = function () {
-  this.total += this.priceSize + this.priceTopping;
+  total += priceSize + priceTopping;
 };
 
 // frontend
@@ -47,13 +49,12 @@ $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
 
+    pizza.feeds = parseInt($("input:radio[name=size]:checked").val());
     pizza.priceFeed();
-    pizza.priceTop();
-    pizza.priceAll();
-    
-  $("#sizes").text(pizza.priceSize);
-  $("#toppings").append(pizza.priceTopping);
-  $("#orderResult").text(pizza.total);
+
+  $("#sizes").text(pizza.feeds);
+  $("#toppings").text(pizza.priceSize);
+
   });
 });
 
@@ -61,12 +62,3 @@ $(document).ready(function() {
 
 
 //
-// // topping option one
-//   if(this.topping = 1) {
-//     pizzaBasePrice += 1;
-//   } else if (this.topping = 2) {
-//     pizzaBasePrice += 2;
-//   } else if (this.topping = 3) {
-//     pizzaBasePrice += 3;
-//   } else if (this.topping = 4) {
-//     pizzaBasePrice += 4;
